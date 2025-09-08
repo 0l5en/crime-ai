2781b74dee0d933e34e49055faccaa9908b2ebce
ole <s.ole@web.de>
2025-09-08 11:43:16 +0200

    feat: favicon and allow PATCH Method for CORS

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -2,6 +2,6 @@
 export const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
-  'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT, DELETE',
+  'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT, DELETE, PATCH',
   'Access-Control-Max-Age': '86400',
 }
